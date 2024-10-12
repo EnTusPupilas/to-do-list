ddb8566e5c9f5429cf60dfe3fc3ec22d39fd8774
David Guerrero <davidguerreroce7@gmail.com>
2024-10-12 12:48:37 -0500

    changes for deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import path from 'path';
 
@@ -7,7 +7,15 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html', // SPA fallback
+			strict: false
+		}),
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '/to-do-list' : ''
+		},
 		alias: {
 			$views: path.resolve('src/infra/view')
 		}
