d83f6f891dd28ab9f29fc2799e0dd07beec40d50
David Guerrero <davidguerreroce7@gmail.com>
2024-10-12 13:13:08 -0500

    change

--- svelte.config.js
+++ svelte.config.js
@@ -4,21 +4,21 @@ import path from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
+  preprocess: vitePreprocess(),
 
-	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			strict: false
-		}),
-		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/to-do-list' : ''
-		},
-		alias: {
-			$views: path.resolve('src/infra/view')
-		}
-	}
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      strict: false
+    }),
+    paths: {
+      base: process.env.NODE_ENV === 'production' ? '/to-do-list' : ''
+    },
+    alias: {
+      $views: path.resolve('src/infra/view')
+    }
+  }
 };
 
 export default config;
